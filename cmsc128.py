da7000c4a1eb7a66b6a99a3b2606a4389789a7cd
pericodan <perico.dionisio@gmail.com>
2016-03-07 22:28:09 +0800

    Edited functions for defensive programming

--- cmsc128.py
+++ cmsc128.py
@@ -1,16 +1,24 @@
+#	Perico Dan B. Dionisio
+#	CMSC 128 AB-1L
+
+
 def getHammingDistance(str1, str2):
 	distance = 0
-	if len(str1)==len(str2):
-		for i in range (0, len(str1)):
-			if str1[i]!=str2[i]:
-				distance = distance + 1
-		return distance
-	else:
-		return "Error: not the same length "
+
+	if type(str1) != str or type(str2) != str or len(str1) != len(str2):
+		return "Error: Bad argument"
+	for i in range (0, len(str1)):
+		if str1[i]!=str2[i]:
+			distance = distance + 1
+	return distance
+
 
 def countSubstrPattern(str1, str2):
 	count = 0
 	checker = 1
+
+	if type(str1) != str or type(str2) != str:
+		return "Error: Bad argument"
 	for i in range (0, len(str1)-len(str2)+1):
 		for j in range (0, len(str2)):
 			if str1[i+j]!=str2[j]:
@@ -20,48 +28,60 @@ def countSubstrPattern(str1, str2):
 		checker = 1
 	return count
 
-def isValidStrings(str, alphabet):
-	for i in range (0, len(str)):
-		if str[i] not in alphabet:
+
+def isValidStrings(str1, alphabet):
+	if type(str1) != str or type(alphabet) != str:
+		return "Error: Bad argument"
+	for i in range (0, len(str1)):
+		if str1[i] not in alphabet:
 			return False
 	return True
 
-def getSkew(str, n):
+
+def getSkew(str1, n):
 	answer = 0
 	numOfG = 0
 	numOfC = 0
-	if n < 1 or n > len(str):
+	if type(str1) != str or type(n) != int or isValidStrings(str1, "ACGTU") == False:
+		return "Error: Bad argument"
+	if n < 1 or n > len(str1):
 		return "Error"
 	else:
 		for i in range (0, n):
-			if str[i] == "G":
+			if str1[i] == "G":
 				numOfG = numOfG + 1
-			if str[i] == "C":
+			if str1[i] == "C":
 				numOfC = numOfC + 1
 		return numOfG - numOfC
 
-def getMaxSkewN(str, n):
+
+def getMaxSkewN(str1, n):
 	max = False
+
+	if type(str1) != str or type(n) != int or isValidStrings(str1, "ACGTU") == False:
+		return "Error: Bad argument"
+	if n < 1 or n > len(str1):
+		return "Error"
 	for i in range(1, n+1):
-		temp = getSkew(str, i)
+		temp = getSkew(str1, i)
 		if max == False:
 			max = temp
 		if max < temp:
 			max = temp
 	return max
 
-def getMinSkewN(str, n):
+
+def getMinSkewN(str1, n):
 	min = False
+
+	if type(str1) != str or type(n) != int or isValidStrings(str1, "ACGTU") == False:
+		return "Error: Bad argument"
+	if n < 1 or n > len(str1):
+		return "Error"
 	for i in range(1, n+1):
-		temp = getSkew(str, i)
+		temp = getSkew(str1, i)
 		if min == False:
 			min = temp
 		if min > temp:
 			min = temp
 	return min
-
-print getHammingDistance("AACCTT","GGCCTT")
-print countSubstrPattern("AATATATAGG","ACTGACTGACTG")
-print isValidStrings("AAGGCTATGC", "ACGT")
-print getMaxSkewN("GGCCAC", 1)
-print "Edit: Defensive programming"
